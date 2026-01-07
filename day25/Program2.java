f11bb5b7e18afc90df51ddc60acc1ac1cd834570
neer <ayushkumar767@gmail.com>
2026-01-07 11:43:46 +0530

    add array and string programs

---
+++ day25/Program2.java
@@ -0,0 +1,15 @@
+public class Program2 {
+	public static void filterString(String s) {
+		String res = "";
+
+		for(int i=0; i<s.length(); i++){		
+			char ch = s.charAt(i);
+			if(ch>='a' && ch<='z' || ch>='A' && ch<='Z')
+				res+=ch;
+		}
+		System.out.println(res);
+	}
+    public static void main(String[] args) {
+        filterString("babc124ABC");
+    }
+}
